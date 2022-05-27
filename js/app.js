629be0db2b9b5e139c2a2a59014554fa5402ff1f
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-27 18:02:35 +0300

    Fixed the torture of css animations

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
 const logIn = [document.querySelector('.log-in .log-in'), ...document.querySelectorAll('.fa-xmark')]
 const formBg = document.querySelector('.form-bg')
-const forms = document.querySelectorAll('.form-container')
-const signIn = document.querySelectorAll('[step]')
+const formsContainers = document.querySelectorAll('.form-container')
+const steps = document.querySelectorAll('[step]')
 
 for (let items of logIn)
     items.addEventListener('click', (e) => {
         formBg.classList.toggle('hidden')
     })
 
-signIn.addEventListener('click', () => {
-    for (let form of forms)
-        form.classList.toggle('translate')
+steps[0].addEventListener('click', () => {
+    console.log('khgara')
+    formsContainers[0].classList.add('fade')
+    if (formsContainers[0].classList.contains('slide'))
+        formsContainers[0].classList.remove('slide')
+    formsContainers[0].addEventListener('animationend', () => {
+        formsContainers[0].classList.add('translate')
+        formsContainers[1].classList.remove('translate')
+        formsContainers[1].classList.add('slide')
+
+    }, { once: true })
+})
+steps[1].addEventListener('click', () => {
+    formsContainers[0].classList.remove('fade')
+    formsContainers[1].classList.remove('slide')
+    formsContainers[1].classList.add('fade')
+    formsContainers[1].addEventListener('animationend', () => {
+        console.log(';2gre bhayete')
+        formsContainers[1].classList.add('translate')
+        formsContainers[0].classList.remove('translate')
+        formsContainers[0].classList.add('slide')
+    }, { once: true })
 })
