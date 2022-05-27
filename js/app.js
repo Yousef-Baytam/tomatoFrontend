9b40b672f0ab9334ec00465d997f1e9af4133ddb
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-27 14:32:48 +0300

    Added sign in for html

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-const logIn = document.querySelector('.log-in .log-in')
+const logIn = [document.querySelector('.log-in .log-in'), document.querySelector('.fa-xmark')]
 const formBg = document.querySelector('.form-bg')
 
-logIn.addEventListener('click', (e) => {
-    formBg.classList.toggle('hidden')
-})
+for (let items of logIn)
+    items.addEventListener('click', (e) => {
+        formBg.classList.toggle('hidden')
+    })
