1c17dd96e5b3c1f8fd96c3044805ce73b3629008
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-27 17:06:43 +0300

    Translate Attempt to animate the login and sign in forms

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-const logIn = [document.querySelector('.log-in .log-in'), document.querySelectorAll('.fa-xmark')[0], document.querySelectorAll('.fa-xmark')[1]]
+const logIn = [document.querySelector('.log-in .log-in'), ...document.querySelectorAll('.fa-xmark')]
 const formBg = document.querySelector('.form-bg')
+const forms = document.querySelectorAll('.form-container')
+const signIn = document.querySelectorAll('[step]')
 
 for (let items of logIn)
     items.addEventListener('click', (e) => {
         formBg.classList.toggle('hidden')
     })
+
+signIn.addEventListener('click', () => {
+    for (let form of forms)
+        form.classList.toggle('translate')
+})
