b6e3719c62f4eaa1eb3535cbd0ef7dd48dd006fe
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-28 21:19:17 +0300

    Added the edit form to the profile view instead of restaurants one

--- js/edit.js
+++ js/edit.js
@@ -1,22 +1,25 @@
 class Edit extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `<div class="card-container">
-        <div class="card">
-            <div class="img">
-                <img src="../assets/jay-wennington-N_Y88TWmGwA-unsplash.jpg" alt="">
-            </div>
-            <div class="title">
-                Hakoona Matata
-            </div>
-            <div class="rating">
-                <div>Rating </div>
-                <div><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
-                        class="fa-solid fa-star"></i> <i class="fa-regular fa-star"></i><i
-                        class="fa-regular fa-star"></i></div>
-            </div>
-            <div>
-                <div class="cuisine">Cuisine: catfood</div>
-                <div class="leave-a-review">Leave a Review</div>
+        this.innerHTML = `<div class="form-bg hidden">
+        <div class="steps-container">
+            <div class="form-container">
+                <form action="#" method="POST" class="form">
+                    <div>
+                        <i class="fa-solid fa-xmark"></i>
+                        <input type="text" name="First" placeholder="First Name">
+                    </div>
+                    <div>
+                        <input type="text" name="Last" placeholder="Last Name">
+                    </div>
+                    <div>
+                        <input type="number" name="Phone" placeholder="Phone Number">
+                    </div>
+                    <div>
+                        <label dob-label for="dob"> Date of Birth</label>
+                        <input type="Date" name="dob" id='dob'>
+                    </div>
+                    <input type="submit" name="submit" value="Submit">
+                </form>
             </div>
         </div>
     </div>`
