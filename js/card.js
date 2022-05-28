44790c739a9bfde27986365fe150ae2cb8028d2a
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-28 16:47:17 +0300

    Changed the card element creation approach

---
+++ js/card.js
@@ -0,0 +1,36 @@
+class Card extends HTMLElement {
+    rating() {
+        let stars = ``
+        for (let i = 0; i < 5; i++) {
+            if (i < this.getAttribute('rating'))
+                stars += `<i class="fa-solid fa-star"></i>`
+            else
+                stars += `<i class="fa-regular fa-star"></i>`
+        }
+        return stars
+    }
+    connectedCallback() {
+        this.innerHTML = `<div class="card-container">
+        <div class="card">
+            <div class="img">
+                <img src="${ this.getAttribute('imgSrc') }" alt="">
+            </div>
+            <div class="title">
+            ${ this.getAttribute('title') }
+            </div>
+            <div class="rating">
+                <div>Rating </div>
+                <div>
+                ${ this.rating }
+                </div>
+            </div>
+            <div>
+                <div class="cuisine">Cuisine: ${ this.getAttribute('cuisine') }</div>
+                <div class="leave-a-review">Leave a Review</div>
+            </div>
+        </div>
+    </div>`
+    }
+}
+
+customElements.define('my-card', Card)
--- js/elements.js
+++ js/elements.js
@@ -1,85 +1,35 @@
-const navbar = `
-        <nav>
-            <div>
-                <div>
-                    Tomato
-                </div>
-                <div>
-                    <a href="./views/restaurants.html">All restaurants</a>
-                </div>
-            </div>
-            <div class="log-in">
-                <div class="log-in">
-                    Login
-                </div>
-                <div class="log-in d-none">
-                    <div>username</div>
-                    <div>profile img</div>
-                </div>
-            </div>
-        </nav>
-        <div class="form-bg hidden">
-            <div class="steps-container">
-                <div class="form-container">
-                    <form action="#" method="POST" class="form">
-                        <div>
-                            <i class="fa-solid fa-xmark"></i>
-                            <input type="Email" name="Email" placeholder="Email">
-                        </div>
-                        <div>
-                            <input type="password" name="Passwrod" placeholder="Password">
-                        </div>
-                        <input type="submit" name="submit" value="Log in">
-                        <div class="signIn" step>Create New Account</div>
-                    </form>
-                </div>
-                <div class="form-container translate">
-                    <form action="#" method="POST" class="form">
-                        <div>
-                            <i class="fa-solid fa-xmark"></i>
-                            <input type="Email" name="Email" placeholder="Email">
-                        </div>
-                        <div>
-                            <input type="password" name="Passwrod" placeholder="Password">
-                        </div>
-                        <input type="submit" name="submit" value="Sign In">
-                        <div class="back" step>Back</div>
-                    </form>
-                </div>
-            </div>
-        </div>`
+// const createCard = (imageSrc, title, rating, cuisine) => {
+//     let stars = ``
+//     for (let i = 0; i < 5; i++) {
+//         if (i < rating)
+//             stars += `<i class="fa-solid fa-star"></i>`
+//         else
+//             stars += `<i class="fa-regular fa-star"></i>`
+//     }
+//     return (
+//         `<div class="card-container">
+//         <div class="card">
+//             <div class="img">
+//                 <img src="${ imageSrc }" alt="">
+//             </div>
+//             <div class="title">
+//                 ${ title }
+//             </div>
+//             <div class="rating">
+//                 <div>Rating </div>
+//                 <div>
+//                     ${ stars }
+//                 </div>
+//             </div>
+//             <div>
+//                 <div class="cuisine">Cuisine: ${ cuisine }</div>
+//                 <div class="leave-a-review">Leave a Review</div>
+//             </div>
+//         </div>
+//     </div>`
+//     )
+// }
 
-const createCard = (imageSrc, title, rating, cuisine) => {
-    let stars = ``
-    for (let i = 0; i < 5; i++) {
-        if (i < rating)
-            stars += `<i class="fa-solid fa-star"></i>`
-        else
-            stars += `<i class="fa-regular fa-star"></i>`
-    }
-    return (
-        `<div class="card-container">
-        <div class="card">
-            <div class="img">
-                <img src="${ imageSrc }" alt="">
-            </div>
-            <div class="title">
-                ${ title }
-            </div>
-            <div class="rating">
-                <div>Rating </div>
-                <div>
-                    ${ stars }
-                </div>
-            </div>
-            <div>
-                <div class="cuisine">Cuisine: ${ cuisine }</div>
-                <div class="leave-a-review">Leave a Review</div>
-            </div>
-        </div>
-    </div>`
-    )
-}
 const conatiner = document.querySelector('.container')
 
 const cardGenerator = (api) => {
@@ -90,8 +40,8 @@ const cardGenerator = (api) => {
                 conatiner.appendChild(document.createElement('div').innerHTML('<h1>No restaurants matching this criteria</h1>'))
             for (let i of res) {
                 let { image, title, rating, category } = i
-                let card = createCard(image, title, rating, category)
-                conatiner.appendChild(card)
+                let card = `<my-card imgSrc='${ image }' title='${ title }' cuisine='${ category }' rating='rating'/>`
+                conatiner.insertAdjacentHTML('beforeend', card)
             }
         }).catch((e) => {
             console.log(e)
