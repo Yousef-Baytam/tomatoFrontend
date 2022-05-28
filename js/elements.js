1685ae09d61d9b562f2f0e95120f9ee36f340ed2
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-28 17:02:25 +0300

    Fixed the style of the top bar in the profile view

--- js/elements.js
+++ js/elements.js
@@ -40,7 +40,7 @@ const cardGenerator = (api) => {
                 conatiner.innerHTML = '<h1>No restaurants matching this criteria</h1>'
             for (let i of res) {
                 let { image, title, rating, category } = i
-                let card = `<my-card imgSrc='${ image }' title='${ title }' cuisine='${ category }' rating='rating'/>`
+                let card = `<my-card imgSrc='${ image }' title='${ title }' cuisine='${ category }' rating=${ rating }/>`
                 conatiner.insertAdjacentHTML('beforeend', card)
             }
         }).catch((e) => {
