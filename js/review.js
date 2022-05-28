43421a541f005a151791852f9e0ea362cb8810ee
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-28 22:23:19 +0300

    Added attributes to review form

--- js/review.js
+++ js/review.js
@@ -1,28 +1,34 @@
 class Review extends HTMLElement {
+    rating() {
+        let stars = ``
+        for (let i = 0; i < 5; i++) {
+            if (i < this.getAttribute('rating'))
+                stars += `<i class="fa-solid fa-star"></i>`
+            else
+                stars += `<i class="fa-regular fa-star"></i>`
+        }
+        return stars
+    }
     connectedCallback() {
         this.innerHTML = `<div class="form-bg hidden" id="review">
         <div class="steps-container">
             <div class="form-container" review>
                 <div class="card">
                     <div class="img">
-                        <img src="../assets/jay-wennington-N_Y88TWmGwA-unsplash.jpg" alt="">
+                        <img src="${ this.getAttribute('imgSrc') }" alt="">
                     </div>
                     <div class="title">
-                        Hakoona Matata
+                    ${ this.getAttribute('title') }
                     </div>
                     <div class="description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque accusamus dolorem
-                        necessitatibus enim quae ab officia iusto repudiandae. Beatae quibusdam ea natus doloremque
-                        voluptatum fuga, vero nostrum ipsa porro totam!
+                        ${ this.getAttribute('imgSrc') }
                     </div>
                     <div class="rating">
                         <div>Rating </div>
-                        <div><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
-                                class="fa-solid fa-star"></i> <i class="fa-regular fa-star"></i><i
-                                class="fa-regular fa-star"></i></div>
+                        <div>${ this.rating }</div>
                     </div>
                     <div>
-                        <div class="cuisine">Cuisine: catfood</div>
+                        <div class="cuisine">Cuisine: ${ this.getAttribute('cuisine') }</div>
                     </div>
                 </div>
                 <form action="#" method="POST" class="form" review-form>
