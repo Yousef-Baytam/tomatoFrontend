a274f1ad44f5102de9b78fe38a6e0781d2dd4d7c
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-28 20:40:27 +0300

    Changed the userprofile info format

--- js/user.js
+++ js/user.js
@@ -4,14 +4,24 @@ class User extends HTMLElement {
         <img src="${ this.getAttribute('imgSrc') ? this.getAttribute('imgSrc') : `../assets/blank-profile.webp` }" alt="user Image">
     </div>
     <div info>
-        <div>First Name: ${ this.getAttribute('name') }</div>
-        <div>Last Name: ${ this.getAttribute('lastName') }</div>
-        <div>Email: ${ this.getAttribute('email') }</div>
-        <div>Phone: ${ this.getAttribute('phone') }</div>
-        <div>Location: ${ this.getAttribute('location') }</div>
-        <div>Date of Birth: ${ this.getAttribute('dob') }</div>
+        <div>First Name:</div>
+        <div>Last Name:</div>
+        <div>Email:</div>
+        <div>Phone:</div>
+        <div>Location:</div>
+        <div>Date of Birth:</div>
         <button class='edit'>Edit</button>
-    </div></div>`
+    </div>
+    <div info two>
+        <div>${ this.getAttribute('name') }</div>
+        <div>${ this.getAttribute('lastName') }</div>
+        <div>${ this.getAttribute('email') }</div>
+        <div>${ this.getAttribute('phone') }</div>
+        <div>${ this.getAttribute('location') }</div>
+        <div>${ this.getAttribute('dob') }</div>
+        <div extra></div>
+    </div>
+    </div>`
     }
 }
 
