3e57f6bcc125d2447f5e84104fe3ad64637ad0b8
Khaled Faour <khaledfaour2@gmail.com>
2022-05-31 09:15:39 +0300

    Fixed User Item Action Buttons Functionlaity

--- js/admin-users.js
+++ js/admin-users.js
@@ -1,3 +1,55 @@
-const userEditClose = document.getElementById('user-edit-close').onclick = (e)=>{
-    document.getElementsByTagName('my-edit')[0].children[0].classList.add('hidden')
+window.onload= ()=>{
+    fetchData()
 }
+
+const fetchData = async ()=>{
+    let users = []
+    await axios.get('http://127.0.0.1/tomatobackend/getUsers.php').then(response=>{
+        users = response.data
+    })
+
+    if(users.length > 0){
+        const container = document.getElementsByClassName('users-admin-container')[0];
+        users.map(user=>
+            container.innerHTML +=`
+            <user-item 
+                id="${user.id}" 
+                fname="${user.first_name}" 
+                lname="${user.last_name}" 
+                email= "${user.email}" 
+                phone="${user.phone_number}" 
+                image="${user.profile_pic}"
+                dob="${user.dob}"
+                status = "${user.status}"
+                city = "${user.cities_id}"
+                type= "${user.user_types_id}"></user-item>`
+        )
+    }
+    
+}
+
+async function remove(id){
+    const form = new FormData();
+    form.append('id', id)
+    await axios.post('http://127.0.0.1/tomatobackend/removeUser.php', form).then(response=>
+        location.reload()
+    )
+ }
+ 
+ async function ban(id){
+    const form = new FormData();
+    form.append('status', 'banned')
+    form.append('id', id)
+    await axios.post('http://127.0.0.1/tomatobackend/updateUserStatus.php', form).then(response=>
+        location.reload()
+    )
+ }
+
+ async function activate(id){
+    const form = new FormData();
+    form.append('status', 'active')
+    form.append('id', id)
+    await axios.post('http://127.0.0.1/tomatobackend/updateUserStatus.php', form).then(response=>
+        location.reload()
+    )
+ }
--- js/user-item.js
+++ js/user-item.js
@@ -1,42 +1,43 @@
 class UserItem extends HTMLElement{
 
 
-    fullname = this.getAttribute('fname') + " " + this.getAttribute('lname')
+    id = this.getAttribute('id')
+    fname = this.getAttribute('fname')
+    lname = this.getAttribute('lname')
+    email = this.getAttribute('email')
+    phone_number = this.getAttribute('phone')
+    profile_pic = this.getAttribute('image')
+    dob = this.getAttribute('dob')
+    user_types_id = this.getAttribute('type')
+    cities_id = this.getAttribute('city')
+    status = this.getAttribute('status')
 
     connectedCallback(){
         this.innerHTML=`
             <div class="admin-user-card">
                 <div class="profile-pic">
-                    <img src="/assets/blank-profile.webp"/>
+                    <img src="${this.getAttribute("image")}"/>
                 </div>
                 <div style="width:100%; margin-left: 24px">
                     <div class='head'>
                         <div class="title">
-                            <h3>${this.fullname}</h3>
+                            <h3>${this.fname} ${this.lname}</h3>
                         </div>
                         <div class="action-buttons">
-                            <span onclick="editRest()"><i class="fa-solid fa-pen-to-square"></i></span>
-                            <span onclick="remove()"><i class="fa-solid fa-trash-can"></i></span>
-                        </div>
+                            <span onclick="remove(${this.id})"><i class="fa-solid fa-trash-can"></i></span>`+
+                            (this.status != "banned" ? `<span onclick="ban(${this.id})" title="ban"><i class="fa-solid fa-ban"></i></span>`:'') +
+                            (this.status == "banned" ? `<span onclick="activate(${this.id})" title="unban" ><i class="fa-solid fa-check"></i></span>` :'')+
+                        `</div>
                     </div>
                     <div class = "content">
                         <div class="description">
-                            <p>${this.getAttribute('dob') || 'Date of birth'}</p>
+                            <p>${this.dob || 'Date of birth'}</p>
                         </div>
                     </div>
                 </div>
             </div>
         `
     }
-}
-
-
-
-function editRest(){
-    document.getElementsByTagName('my-edit')[0].children[0].classList.remove('hidden')
-}
-function remove(){
-    return console.log("remove")
 }
 
 customElements.define('user-item', UserItem);
